05615d6ec11eaf362a1bade04ba1137e9d334590
Eric Hokanson <hokanson@gmail.com>
2017-01-10 19:34:35 -0700

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dyndns53
-Dynamic DNS with Route53
+My take on dynamic DNS using AWS Route 53.
+Features support for both IPV4 A records and IPV6 AAAA records.
+Works with Python 2.6+.
+
+## Quick Start
+Setup your AWS credentials. There are many ways to do this, see:
+https://boto3.readthedocs.io/en/latest/guide/quickstart.html#configuration
+
+Install Python requirements:
+```
+$ pip install -r requirements.txt
+```
+
+At the top of the script there are two variables you must change, `domain` and `subdomain`.
+You must already have the hosted zone setup in your AWS account that matches `domain`.
+
+Run the script `python dyndns53.py`. It works best when run on a cron every hour.
---
+++ dyndns53.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import logging
+import boto3
+import requests
+from datetime import datetime
+
+domain = 'example.com'
+subdomain = 'home'
+
+def upsert(zone_id, rtype, value):
+    client.change_resource_record_sets(
+        HostedZoneId=zone_id,
+        ChangeBatch={
+            'Changes':[{
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': fqdn,
+                    'Type': rtype,
+                    'TTL': 900,
+                    'ResourceRecords':[{
+                        'Value': value
+                    }]
+                }
+            }]
+        })
+
+
+def main():
+    my_matches = {'A': [], 'AAAA': []}
+    updated = False
+
+    client = boto3.client('route53')
+
+    zone = client.list_hosted_zones_by_name(DNSName=domain)
+    if len(zone['HostedZones']) != 1:
+        logger.critical("Couldn't find configured domain in route53!")
+        exit(1)
+    zone_id = zone['HostedZones'][0]['Id']
+
+    rrs = client.list_resource_record_sets(HostedZoneId=zone_id)
+    for record in rrs['ResourceRecordSets']:
+        if record['Name'] == fqdn and record['Type'] in ['A','AAAA']:
+            my_matches[record['Type']] = map(lambda x: x['Value'], record['ResourceRecords'])
+
+    try:
+        ipv4 = requests.get('http://ipv4.icanhazip.com', timeout=15).text.rstrip()
+        if ipv4 not in my_matches['A']:
+            logger.info('Updating {0}: {1} -> {2}'.format(fqdn, my_matches['A'], ipv4))
+            upsert(zone_id, 'A', ipv4)
+            updated = True
+    except:
+        logger.warning('Couldn\'t detect your IPV4 address.')
+
+    try:
+        ipv6 = requests.get('http://ipv6.icanhazip.com', timeout=15).text.rstrip()
+        if ipv6 not in my_matches['AAAA']:
+            logger.info('Updating {0}: {1} -> {2}'.format(fqdn, my_matches['AAAA'], ipv6))
+            upsert(zone_id, 'AAAA', ipv6)
+            updated = True
+    except:
+        logger.warning('Couldn\'t detect your IPV6 address.')
+
+    if updated:
+        datestr = '"Last update {0}."'.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M'))
+        logger.info(datestr)
+        upsert(zone_id, rtype='TXT', value=datestr)
+    else:
+        logger.info('"{0}" is current.'.format(fqdn))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger('dyndns53')
+    logger.setLevel(logging.DEBUG)
+    fqdn = '{0}.{1}.'.format(subdomain.strip('.'), domain.strip('.'))
+    main()
