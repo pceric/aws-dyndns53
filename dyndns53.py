33ad4bbb104553078cd49073af3c67df8d1f6a6d
Eric Hokanson <hokanson@gmail.com>
2017-09-23 14:43:39 -0600

    Fix function call

--- dyndns53.py
+++ dyndns53.py
@@ -8,7 +8,7 @@ from datetime import datetime
 domain = 'example.com'
 subdomain = 'home'
 
-def upsert(zone_id, rtype, value):
+def upsert(client, zone_id, rtype, value):
     client.change_resource_record_sets(
         HostedZoneId=zone_id,
         ChangeBatch={
@@ -47,7 +47,7 @@ def main():
         ipv4 = requests.get('http://ipv4.icanhazip.com', timeout=15).text.rstrip()
         if ipv4 not in my_matches['A']:
             logger.info('Updating {0}: {1} -> {2}'.format(fqdn, my_matches['A'], ipv4))
-            upsert(zone_id, 'A', ipv4)
+            upsert(client, zone_id, 'A', ipv4)
             updated = True
     except:
         logger.warning('Couldn\'t detect your IPV4 address.')
@@ -56,7 +56,7 @@ def main():
         ipv6 = requests.get('http://ipv6.icanhazip.com', timeout=15).text.rstrip()
         if ipv6 not in my_matches['AAAA']:
             logger.info('Updating {0}: {1} -> {2}'.format(fqdn, my_matches['AAAA'], ipv6))
-            upsert(zone_id, 'AAAA', ipv6)
+            upsert(client, zone_id, 'AAAA', ipv6)
             updated = True
     except:
         logger.warning('Couldn\'t detect your IPV6 address.')
@@ -64,7 +64,7 @@ def main():
     if updated:
         datestr = '"Last update {0}."'.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M'))
         logger.info(datestr)
-        upsert(zone_id, 'TXT', datestr)
+        upsert(client, zone_id, 'TXT', datestr)
     else:
         logger.info('"{0}" is current.'.format(fqdn))
 
